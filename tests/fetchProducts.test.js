f33f3f82b63220ed9be0dafad92b4a55d15f1091
Alex <alexsandrolima991@hotmail.com>
2022-07-11 17:31:50 -0300

    Primeiro Requisito

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,35 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it ('testa se "fetchProducts" é uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toEqual('function');
+  });
+
+  it ('testa se o fetch é chamado', async () => {
+    expect.assertions(1);
+    const callFunction = await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
+
+  it ('testa se o fetch retorna o endpoint', async () => {
+    expect.assertions(1);
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+    await fetchProducts('computador');
+
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it ('testa se ao chamar com o param computador retorna igual ao computadorSearch', async () => {
+    expect.assertions(1);
+    const func = await fetchProducts('computador');
+    expect(func).toMatchObject(computadorSearch);
+  });
+
+  it ('testa se chama o erro', async () => {
+    const callFunction = await fetchProducts();
+    expect(callFunction).toEqual(new Error('You must provide an url'));
+  });
+  // fail('Teste vazio');
 });
